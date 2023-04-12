8027eae5c94ab35ad8e07b1c1d5bbb08739257ca
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2023-04-13 02:22:17 +0430

    Get connection id.

--- src/Presentations/WebUi/Hubs/ChatHub.cs
+++ src/Presentations/WebUi/Hubs/ChatHub.cs
@@ -4,8 +4,14 @@ namespace WebUi.Hubs;
 
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string user, string message)
+    public async Task SendBroadcastMessage(string user, string message)
     {
         await Clients.All.SendAsync("RecieveMessage", user, message);
     }
+    public async Task SendMessage(string user, string receiverConnectionId, string message)
+    {
+        await Clients.Client(receiverConnectionId).SendAsync("RecieveMessage", user, message);
+    }
+
+    public string UserOnConnected() => Context.ConnectionId;
 }
